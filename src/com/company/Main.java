effe12cfe2ca25e280761b90b988369f1c87cf78
DashaDrumFury <dashadasha560@gmail.com>
2021-12-25 17:44:08 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Drumming is my life");
+
+
+        test();
+
+    }
+    public static void test(){
+        System.out.println("Y TYT");
+    }
+
+}
