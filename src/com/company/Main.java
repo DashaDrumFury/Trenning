8f0cf740742260a76683981b79ef90881b29fae9
DashaDrumFury <dashadasha560@gmail.com>
2021-12-26 01:28:45 +0700

    refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,14 @@ public class Main {
 	// write your code here
         System.out.println("Drumming is my life");
 
+        test();
+        test2();
 
-        test();
-        test();
     }
+
+    private static void test2() { System.out.println("Kak horosho");
+    }
+
     public static void test(){
         System.out.println("Y TYT");
     }
