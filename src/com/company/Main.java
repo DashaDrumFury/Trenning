e27f0dc1335a8add77c772eef18a706c88e5ab40
DashaDrumFury <dashadasha560@gmail.com>
2021-12-26 01:48:29 +0700

    ref2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,14 +8,17 @@ public class Main {
 
         test();
         test2();
+        test3();
+    }
 
+    private static void test3() { System.out.println("Doma");
     }
 
     private static void test2() { System.out.println("Kak horosho");
     }
 
     public static void test(){
-        System.out.println("Y TYT");
+        System.out.println("YA TYT");
     }
 
 }
