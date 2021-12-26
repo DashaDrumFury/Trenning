b9631b00e4ba83210f88dbca535570c930c75434
DashaDrumFury <dashadasha560@gmail.com>
2021-12-26 14:34:35 +0700

    ref2

---
+++ src/com/company/ifelse.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = (a == 5) ? 1 : 2;
+
+        if (b == 0) {
+            System.out.println("statement IF");
+        } else if (b == 1 ) {
+            System.out.println("statement ELSE-IF 1");
+        } else if (b == 2 ) {
+            System.out.println("statement IF 2");
+        } else {
+            System.out.println("statement ELSE");
+        }
+
+        if (b > 0) {
+            System.out.println("b > 0");
+        } else {
+            System.out.println("b <= 0");
+        }
+    }
+}
